48d2d074efb0d95b1234e23b878e79336ed0d134
alexxljm-lgtm <alexxljm@gmail.com>
2026-02-27 13:41:19 +0100

    feat: setup firebase core configuration for android

--- hiddo/android/app/build.gradle.kts
+++ hiddo/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -14,7 +15,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
+    
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
